d3e773d78b5b9453ffed6dfbbd40ee8b3c41dbe3
MacBook <macbook@MacBook-MacBook.local>
2022-04-19 08:53:27 +0200

    button for various counting steps added

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function init() {
   // accessing buttons
+  let buttonsGroup = document.querySelector(".buttons-group");
   let increaseButton = document.querySelector("#increase-button");
   let resetButton = document.querySelector("#reset-button");
   let decreaseButton = document.querySelector("#decrease-button");
-  let buttonsGroup = document.querySelector(".buttons-group");
 
   // listening to a click event on buttons group (defining event listener)
   buttonsGroup.addEventListener("click", update, true);
@@ -11,23 +11,37 @@ function init() {
   // change counter displayed text when event handler called
   let counterText = document.querySelector(".counter");
 
+	const defaultCounterValue = 0;
+
   let state = {
-    counter: 0,
+    counter: defaultCounterValue,
     step: 1,
   };
+  // changign step size from a list
+  let stepSelector = document.querySelector("#step-select");
+
+  stepSelector.addEventListener("change", onSelectedStep, true);
+
+  function onSelectedStep(event) {
+    counterText.textContent = defaultCounterValue;
+		state.counter = defaultCounterValue;
+    state.step = parseInt(event.target.value);
+  }
 
   // defining event handler function
   function update(event) {
     if (event.target == increaseButton) {
-      state.counter += state.step;
+			state.counter += state.step;
     }
 
     if (event.target == resetButton) {
-      state.counter = 0;
+      state.counter = defaultCounterValue;
     }
 
     if (event.target == decreaseButton) {
-      state.counter -= state.step && state.counter > 0;
+      if (state.counter > 0) {
+        state.counter -= state.step;
+      }
     }
 
     counterText.textContent = state.counter;
